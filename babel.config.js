d12bd55aeee3029977c618af4ca3b27aef9a70f1
Josh Rosenstein <32781407+JoshRosenstein@users.noreply.github.com>
2018-10-08 17:03:23 -0400

    Fix Config to work with vscode-jest

---
+++ babel.config.js
@@ -0,0 +1,23 @@
+module.exports = function babelConfig(api){
+  api.cache(true)
+  return{
+    'presets': [
+      ['@babel/preset-env', {
+        'modules': false
+      }],
+      '@babel/preset-react'
+    ],
+    'plugins': [
+      '@babel/plugin-proposal-class-properties'
+    ],
+    'env': {
+      'test': {
+        'presets': [
+          '@babel/preset-env',
+          '@babel/preset-react'
+        ]
+      },
+  
+    }
+  }}
+   
--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,9 @@
 module.exports = {
   transform: {
-  //  '\\.css$': '<rootDir>/test/styleTransform.js',
+    //  '\\.css$': '<rootDir>/test/styleTransform.js',
     '^.+\\.js?$': 'babel-jest'
-  },'testEnvironment': 'node',
-  // watchPlugins: [
-  //   'jest-watch-typeahead/filename',
-  //   'jest-watch-typeahead/testname'
-  // ],
-  testPathIgnorePatterns: ['/node_modules/', '/__fixtures__/','/__utils__/'],
-  // setupTestFrameworkScriptFile: '<rootDir>/test/testSetup.js',
-  coveragePathIgnorePatterns: [
-    '/node_modules/',
-  ]
+  },
+  testEnvironment: 'node',
+  testPathIgnorePatterns: ['/node_modules/', '/__fixtures__/', '/__utils__/'],
+  coveragePathIgnorePatterns: ['/node_modules/']
 }
